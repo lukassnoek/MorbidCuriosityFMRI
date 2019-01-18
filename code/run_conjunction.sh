1dd34385c3848ba9d2c67cd525318fb31071fa1f
Lukas Snoek <lukassnoek@gmail.com>
2019-01-18 16:57:37 +0100

    fix conj code

--- code/run_conjunction.sh
+++ code/run_conjunction.sh
@@ -4,4 +4,4 @@ C1_DIR=$MAIN_DIR/cope1.feat/stats
 C2_DIR=$MAIN_DIR/cope1.feat/stats
 #smoothness: no argument to calculate individual smoothness parameters
 
-bash easythresh_conj.sh -s $C1_DIR/zstat1.nii.gz $C1_DIR/zstat1.nii.gz $MAIN_DIR/mask.nii.gz 2.58 0.05 $MAIN_DIR/bg_image.nii.gz conj_neg_pos
+./easythresh_conj.sh $C1_DIR/zstat1.nii.gz $C2_DIR/zstat1.nii.gz $MAIN_DIR/mask.nii.gz 2.58 0.05 $MAIN_DIR/bg_image.nii.gz conjunction_cue-negpos_contrast-actpas
