45dc461165dca52f8be0008b8fd8171caabd438b
Lukas Snoek <lukassnoek@gmail.com>
2019-01-21 14:17:00 +0100

    Update notebook + update data

--- code/run_conjunction.sh
+++ code/run_conjunction.sh
@@ -5,4 +5,4 @@ C2_DIR=$MAIN_DIR/cope2.feat/stats
 OUTPUT_DIR=$MAIN_DIR/conj_analysis
 #smoothness: no argument to calculate individual smoothness parameters
 
-./easythresh_conj.sh $C1_DIR/zstat1.nii.gz $C2_DIR/zstat1.nii.gz $MAIN_DIR/mask.nii.gz 2.58 0.05 $MAIN_DIR/bg_image.nii.gz conjunction_cue-negpos_contrast-actpas
+./easythresh_conj.sh $C1_DIR/zstat1.nii.gz $C2_DIR/zstat1.nii.gz $MAIN_DIR/mask.nii.gz 2.576 0.05 $MAIN_DIR/bg_image.nii.gz conjunction_cue-negpos_contrast-actpas
