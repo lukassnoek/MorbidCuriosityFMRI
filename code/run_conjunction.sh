46543d94f13291c2263e7ebb6f2f37553360913d
Jurkoppele <j.tekoppele@gmailcom>
2019-01-21 01:39:38 +0100

    changed minor mistake in conj-code

--- code/run_conjunction.sh
+++ code/run_conjunction.sh
@@ -1,7 +1,8 @@
 #set the main feat-directory 
 MAIN_DIR=../results/wholebrain.gfeat
 C1_DIR=$MAIN_DIR/cope1.feat/stats
-C2_DIR=$MAIN_DIR/cope1.feat/stats
+C2_DIR=$MAIN_DIR/cope2.feat/stats
+OUTPUT_DIR=$MAIN_DIR/conj_analysis
 #smoothness: no argument to calculate individual smoothness parameters
 
 ./easythresh_conj.sh $C1_DIR/zstat1.nii.gz $C2_DIR/zstat1.nii.gz $MAIN_DIR/mask.nii.gz 2.58 0.05 $MAIN_DIR/bg_image.nii.gz conjunction_cue-negpos_contrast-actpas
